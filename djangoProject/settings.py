65c95335c1b17c4bbf25682cd710f7007d6ca9d5
Nattalli <62181026+Nattalli@users.noreply.github.com>
2021-05-14 21:47:52 +0300

    Update settings.py

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^4@kto)s&y9lh+6p2t6z8v$hg47q57%5&2)y86jwlwfs51)-4q'
+SECRET_KEY = ****
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -83,12 +83,7 @@ WSGI_APPLICATION = 'djangoProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'ycmzoien',
-        'HOST': 'queenie.db.elephantsql.com',
-        'USER': 'ycmzoien',
-        'PASSWORD': 'CAlmH1CMfUVbcEFZOGneg6Q2SSi8DQ8G',
-        'PORT': '5432'
+        ******
     }
 }
 
